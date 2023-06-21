3ea6517f9c19d1b2ef0d8e00f768d119963086cc
River McCloskey <rivermccloskey99@gmail.com>
2023-06-21 12:58:24 +0100

    feat: trying to use agbabi functions, undef ref

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include "bn_core.h"
+#include "bn_log.h"
+
+#include "agbabi.h"
+#include "aeabi.h"
 
 int main()
 {
     bn::core::init();
+    auto x = __agbabi_sqrt(9);  // this doesn't work
+    auto y = __aeabi_idiv(32, 16);  // this works
+    BN_LOG(x);
+    BN_LOG(y);
 
     while(true)
     {
