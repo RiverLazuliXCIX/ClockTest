e7929f1651590c3574d1f17ef2fbf5c314fa1c61
River McCloskey <rivermccloskey99@gmail.com>
2023-06-24 10:58:31 +0100

    feat: rearranged for demo of issue

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include "bn_sprite_ptr.h"
 #include "bn_vector.h"
 #include "bn_string.h"
-#include "bn_timer.h"
 #include "agbabi.h"
 
 #include "bn_regular_bg_items_background.h"
@@ -14,33 +13,24 @@
 int main()
 {
     bn::core::init();
-    bn::regular_bg_ptr background = bn::regular_bg_items::background.create_bg(0, 0);
 
+    bn::regular_bg_ptr background = bn::regular_bg_items::background.create_bg(0, 0);
     bn::sprite_text_generator text_generator(common::variable_8x16_sprite_font);
+    bn::vector<bn::sprite_ptr, 32> text_sprites;
+
     text_generator.set_center_alignment();
 
-    bn::vector<bn::sprite_ptr, 32> text_sprites;
     if (__agbabi_rtc_init() == 0)
     {
-        __agbabi_datetime_t temp = {0x230622, 0x305113};
-        __agbabi_rtc_setdatetime(temp);
+        __agbabi_rtc_settime(0x000012);
+
+        BN_LOG("rtc time: ", __agbabi_rtc_time());
+
+        text_generator.generate(0, 0, bn::to_string<32>(__agbabi_rtc_time()), text_sprites);
     }
 
-    auto ticks = 0;
     while (true)
     {
-        ticks++;
-
-        if (ticks > 600)
-        {
-            auto rtc_time = __agbabi_rtc_datetime();
-            text_generator.generate(0, 0, bn::to_string<10>(rtc_time[1]), text_sprites);
-            bn::core::update();
-            while (true)
-            {
-            }
-        }
-
         bn::core::update();
     }
 }
